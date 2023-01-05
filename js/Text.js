687e570cc24a1cf04f1a2fb31c5ded866fd89522
Panda98277 <118618400+Panda98277@users.noreply.github.com>
2023-01-05 18:07:20 +0800

    Create Text.js

---
+++ js/Text.js
@@ -0,0 +1,32 @@
+<script type="text/javascript">
+
+window.onload=function()
+
+{
+
+var h2=document.getElementById('h2');
+
+setInterval(function(){
+
+var r=Math.floor(255*Math.random());
+
+var g=Math.floor(255*Math.random());
+
+var b=Math.floor(255*Math.random());
+
+h2.style.color="rgb("+r+","+g+","+b+")";
+
+h2.style.fontSize='250px';
+
+h2.style.textAlign='center';
+
+},1000);
+
+};
+
+</script>
+
+// 作者：pizan
+// 链接：https://www.jianshu.com/p/27a34acead65
+// 来源：简书
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
